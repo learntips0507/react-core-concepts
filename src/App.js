91f8ed62f798e52b8a133a0880dcf783bae9fb0c
learntips0507 <learntips0507@gmail.com>
2020-03-23 04:49:19 +0600

    react props learn end

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  var personName = ['SHAHJAHAN', 'SAMRAT', 'KARIM'];
+  var personFather = ['SHAHEEN', 'HOSSAIN', 'ZAMIL'];
   return (
-    <div className="App">
+    <div class="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Myinfo name= {personName[0]} fatherName= {personFather[0]}></Myinfo>
+        <Myinfo name = {personName[1]} fatherName= {personFather[1]}></Myinfo>
+        <Myinfo name = {personName[2]} fatherName= {personFather[2]}></Myinfo>
       </header>
     </div>
   );
 }
-
+function Myinfo(props){
+  var style = { 
+    color: 'white', 
+    border: '2px solid white', 
+    padding: '5px 30px', 
+    marginTop: '20px',
+    width: '600px'
+  }
+  return (
+  <div style = {style}>
+  <h1>Name : {props.name}</h1>
+  <h3>Father Name : {props.fatherName}</h3>
+  </div>
+  )
+}
 export default App;
